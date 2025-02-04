771e1c7eb3da93daf06810d0d8ad6695c4eee318
leejhy <>
2025-02-04 15:46:22 +0900

    [WEB] FIX sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  outDir: './public',
+  outDir: 'out',
   siteUrl: process.env.SITE_URL,
   autoLastmod: true,
   generateRobotsTxt: true, // (optional)
