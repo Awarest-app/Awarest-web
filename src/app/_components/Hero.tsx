dd363c665181f746688c455cd892ae1aeed2aadc
leejhy <>
2025-02-13 14:07:05 +0900

    [WEB]: download link

--- src/app/_components/Hero.tsx
+++ src/app/_components/Hero.tsx
@@ -1,11 +1,11 @@
 import Image from "next/image";
+import Apple from "~/components/icons/AppleDownload"
+import Link from "next/link";
 
 export default function Hero() {
   return (
-    // 부모 요소에 container-type을 설정합니다.
     <div className="@container">
       <div className="flex flex-col gap-6 px-4 py-10  [@media (min-width:480px)]:gap-8">
-        {/* 컨테이너 너비가 549px 이하일 때 flex-col 적용 */}
         <div className="flex gap-10 max-[700px]:flex-col max-[700px]:gap-10 h-full">
           <div className="flex flex-1 flex-col gap-4 text-left min-h-[400px] max-[700px]:items-center">
             <h1 className="text-foreground text-4xl font-black leading-tight tracking-[-0.033em] [@media (min-width:480px)]:text-5xl [@media (min-width:480px)]:font-black [@media (min-width:480px)]:leading-tight [@media (min-width:480px)]:tracking-[-0.033em]">
@@ -17,13 +17,13 @@ export default function Hero() {
             Answer fun, AI-driven questions daily, track your growth in a unique self reflection diary, and build resilience through gamification.
             </h2>
             <div className="flex mt-auto px-4 w-full items-center justify-center">
-              <button
+              <Link
+                href="https://apple.co/3CD9rlx"
                 className="max-[700px]:w-48 flex w-full min-w-[84px] min-h-[48px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 [@media (min-width:480px)]:h-12 [@media (min-width:480px)]:px-5 bg-primary text-foreground text-sm font-bold leading-normal tracking-[0.015em] [@media (min-width:480px)]:text-base [@media (min-width:480px)]:font-bold [@media (min-width:480px)]:leading-normal [@media (min-width:480px)]:tracking-[0.015em] transition-transform active:scale-[0.99] hover:brightness-[0.85]"
+                target="_blank"
               >
-                <span className="text-[16px] truncate text-[#FAFAFA]">
-                  Coming soon
-                </span>
-              </button>
+                <Apple />
+              </Link>
             </div>
           </div>
           <div className="relative flex-1 rounded-xl max-[700px]:min-h-[350px] [@media (min-width:480px)]:min-w-[400px] [@media (min-width:864px)]:w-full">
--- src/components/SmallButton.tsx
+++ src/components/SmallButton.tsx
@@ -1,11 +1,14 @@
+import Link from "next/link"
 
 export default function SmallButton() {
   return (
-    <button
-    className="flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-primary text-foreground text-sm font-bold leading-normal tracking-[0.015em] 
-    transition-transform active:scale-[0.97] hover:brightness-[0.85]"
+    <Link
+      href="https://apple.co/3CD9rlx"
+      className="flex min-w-[84px] cursor-pointer items-center justify-center overflow-hidden rounded-full h-10 px-4 bg-primary text-foreground text-sm font-bold leading-normal tracking-[0.015em] 
+      transition-transform active:scale-[0.97] hover:brightness-[0.85]"
+      target="_blank"
     >
-      <span className="truncate text-[#fafafa]">Coming soon</span>
-    </button>
+      <span className="truncate text-[#fafafa]">Download Now</span>
+    </Link>
   )
 }
